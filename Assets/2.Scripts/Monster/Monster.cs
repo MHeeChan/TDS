91d3c0f2e48d3e87da95dcae07a68408e8a36fb6
MHeeChan <rednadasc@naver.com>
2025-06-10 03:38:56 +0900

    몬스터 공격 애니메이션 처리

--- Assets/2.Scripts/Monster/Monster.cs
+++ Assets/2.Scripts/Monster/Monster.cs
@@ -3,22 +3,36 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public float moveSpeed = 2f;
+    Animator anim;
     Rigidbody2D rb;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
     {
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Box"))
+        {
+            anim.SetBool("IsAttacking", true);
+        }
+    }
+
+    void OnAttack()
+    {
+        Debug.Log("ATTACK");
     }
 
     void FixedUpdate()
     {
         float heroX = GameManager.Instance.Hero.transform.position.x;
         float myX = rb.position.x;
-        
         rb.velocity = new Vector2(-1 * moveSpeed, rb.velocity.y);
     }
 }
